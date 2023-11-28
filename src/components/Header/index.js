de63b328fe16a75c037a9f30732ee2e7ce3c6fd8
Igor Pereira <pereira.t.igor@gmail.com>
2023-11-28 15:27:51 -0300

    Refactor on Header component to class component, adding props use

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Container } from './styles';
 
-export default function Header(props) {
-  return (
-    <Container>
-      <h1>Igor's JStack Blog</h1>
-      <button type="button" onClick={props.onToggleTheme}>
-      {props.selectedTheme === 'dark' ? '🌞' : '🌚'}
-      </button>
-    </Container>
-  );
+export default class Header extends React.Component{
+  render(){
+
+    const { onToggleTheme, selectedTheme } = this.props;
+
+    return (
+      <Container>
+        <h1>Igor's JStack Blog</h1>
+        <button type="button" onClick={onToggleTheme}>
+        {selectedTheme === 'dark' ? '🌞' : '🌚'}
+        </button>
+      </Container>
+    );
+  }
 }
+
+// export default function Header(props) {
+//   return (
+//     <Container>
+//       <h1>Igor's JStack Blog</h1>
+//       <button type="button" onClick={props.onToggleTheme}>
+//       {props.selectedTheme === 'dark' ? '🌞' : '🌚'}
+//       </button>
+//     </Container>
+//   );
+// }
