fdb601b26fc28a342c5f4c4e6346d8a795934e76
Igor Pereira <pereira.t.igor@gmail.com>
2023-11-28 16:47:14 -0300

    Refactor on App to use class component and adapting states functionallity

--- src/App.js
+++ src/App.js
@@ -6,48 +6,79 @@ import GlobalStyle from './styles/global';
 import Layout from './components/Layout';
 import themes from './styles/themes';
 
-function App() {
-
-  const [theme, setTheme] = useState(JSON.parse(localStorage.getItem('theme')));
-
-  const firstRender = useRef(true);
-
-  const currentTheme = useMemo(() => {
-    return themes[theme] || themes.dark;
-  }, [theme]);
-
-  function handleToggleTheme(){
-    setTheme((prevState) => (
-      prevState === 'dark'
-        ? 'light'
-        : 'dark'
-    ));
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log(props);
+    this.state = {
+      theme: 'dark',
+      posts: [{}],
+    };
   }
 
-  useEffect(() => {
-    if(firstRender.current){
-      firstRender.current = false;
-      return;
-    }
-    localStorage.setItem('theme', JSON.stringify(theme));
-  }, [theme]);
+  render(){
+    const {theme} = this.state;
+    return (
+      <ThemeProvider theme={themes[theme] || themes.dark}>
+       <GlobalStyle />
+         <Layout
+         onToggleTheme={() => {
+          this.setState((prevState) => ({
+            theme: prevState.theme === 'dark'
+              ? 'light'
+              : 'dark'
+            }
+          ));
+         }}
+         selectedTheme={theme}
+         />
+     </ThemeProvider>
+    );
+  }
+}
 
-  return (
-    <ThemeProvider theme={currentTheme}>
-      <GlobalStyle />
-      {/*<button onClick={handleToggleTheme}>Restore Layout</button>
-      {theme === 'dark' && ( */}
-        <Layout
-        onToggleTheme={handleToggleTheme}
-        selectedTheme={theme}
-        />
-      {/*)}
-      {<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />*/}
-    </ThemeProvider>
-  );
-};
+// function App() {
+
+//   const [theme, setTheme] = useState(JSON.parse(localStorage.getItem('theme')));
+
+//   const firstRender = useRef(true);
+
+//   const currentTheme = useMemo(() => {
+//     return themes[theme] || themes.dark;
+//   }, [theme]);
+
+//   function handleToggleTheme(){
+//     setTheme((prevState) => (
+//       prevState === 'dark'
+//         ? 'light'
+//         : 'dark'
+//     ));
+//   }
+
+//   useEffect(() => {
+//     if(firstRender.current){
+//       firstRender.current = false;
+//       return;
+//     }
+//     localStorage.setItem('theme', JSON.stringify(theme));
+//   }, [theme]);
+
+//   return (
+//     <ThemeProvider theme={currentTheme}>
+//       <GlobalStyle />
+//       {/*<button onClick={handleToggleTheme}>Restore Layout</button>
+//       {theme === 'dark' && ( */}
+//         <Layout
+//         onToggleTheme={handleToggleTheme}
+//         selectedTheme={theme}
+//         />
+//       {/*)}
+//       {<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+//       <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+//       <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+//       <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />*/}
+//     </ThemeProvider>
+//   );
+// };
 
 export default App;
