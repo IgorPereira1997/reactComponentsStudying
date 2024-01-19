75ffb88b00df6edae4a050ab0f41db47d06a08b8
Igor Pereira <pereira.t.igor@gmail.com>
2024-01-19 14:18:53 -0300

    Converted ThemeProvider to Class Component

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -2,21 +2,48 @@ import React, { createContext, useState } from "react";
 
 export const ThemeContext = createContext();
 
-export function ThemeProvider({children}){
+export class ThemeProvider extends React.Component{
+  state = {
+    theme: 'dark',
+  };
 
-  const [theme, setTheme] = useState('dark');
-
-    function handleToggleTheme(){
-      setTheme((prevState) => (
-        prevState === 'dark'
-          ? 'light'
-          : 'dark'
-    ));
+  handleToggleTheme = () => {
+    this.setState((prevState) => ({
+      theme: prevState.theme === 'dark'
+        ? 'light'
+        : 'dark'
+    }));
   }
 
-  return(
-    <ThemeContext.Provider value={{theme, handleToggleTheme}}>
-      {children}
-    </ThemeContext.Provider>
-  );
+  render(){
+    return(
+      <ThemeContext.Provider
+        value={{
+          theme: this.state.theme,
+          handleToggleTheme: this.handleToggleTheme
+        }}>
+        {this.props.children}
+      </ThemeContext.Provider>
+    );
+  };
+
 }
+
+// export function ThemeProvider({children}){
+
+//   const [theme, setTheme] = useState('dark');
+
+//     function handleToggleTheme(){
+//       setTheme((prevState) => (
+//         prevState === 'dark'
+//           ? 'light'
+//           : 'dark'
+//     ));
+//   }
+
+//   return(
+//     <ThemeContext.Provider value={{theme, handleToggleTheme}}>
+//       {children}
+//     </ThemeContext.Provider>
+//   );
+// }
